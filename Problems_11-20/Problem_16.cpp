e3146b07a72fa30c3ac931854d723f3856d37e58
vienheno <herrera.victor@outlook.com>
2025-10-16 14:56:55 -0500

    Ejercicio 16 resuelto

--- Problems_11-20/Problem_16.cpp
+++ Problems_11-20/Problem_16.cpp
@@ -12,36 +12,66 @@ using namespace std;
 
 //Función que multiplica strings numéricos por 2
 
-string numeroDuplicado(string cifra)
+string numeroDuplicado(string num_srt)
 {
-    string digito;
-    int lleva = 0;
-    int multx2 = stoi(cifra)*2 + lleva;
-    if (multx2 % 10 > 0) 
+    int tam = num_srt.length() - 1, cifra_int = 0, cifrax2, lleva = 0;
+    string cifra_srt;
+
+    for (int i = tam; i >= 0 ; i--)
     {
-        digito = to_string(multx2 % 10);
-        lleva = 1;
+        cifra_srt = num_srt[i];
+        cifra_int = stoi(cifra_srt);
+        cifrax2 = cifra_int*2 + lleva;
+
+        if (cifrax2 < 10)
+        {
+            num_srt.replace(i, 1, to_string(cifrax2)); //reemplazamos el resultado de la cifra en la misma posicion de num_srt
+            lleva = 0;
+        } else
+        {   
+            num_srt.replace(i, 1, to_string(cifrax2 % 10)); 
+            lleva = 1;
+        }       
     }
-    else
+    
+    if (cifrax2 > 9)
     {
-        digito = to_string(multx2);
-        lleva = 0;
+        num_srt = "1" + num_srt;
+        tam = tam + 1;
     }
-    return digito;
+    
+    return num_srt;
+}
+
+int sumaCifras (string numeroString)
+{
+    int digito_int = 0, suma = 0;
+    string digito_srt;
+    
+    for (int i = 0; i < numeroString.length(); i++)
+    {
+        digito_srt = numeroString[i];
+        digito_int = stoi(digito_srt);
+        suma = suma + digito_int;
+    }
+    
+    return suma;
 }
 
 int main() {
     setlocale(LC_ALL, "es_ES.UTF-8");
     system("cls");
 
-    int pot = 10;
-    string resultado;
-    for (int i = 1; i < pot; i++)
-    {
-        resultado = resultado + numeroDuplicado(to_string(i));
-        
-    }
+    int pot = 0;
+    string resultado = "1";
     
-    cout<<"2 elevado a la "<<pot<<" es: "<<resultado;
+    cout<<endl<<"Ingrese la potencia de 2: ";
+    cin>>pot;
+
+    for (int i = 1; i <= pot ; i++) resultado = numeroDuplicado(resultado);
+
+    //cout<<endl<<"El resultado de 2 elevado a la "<<pot<<" es "<<resultado;
+    cout<<endl<<"La suma de cifras del resultado de 2 elevado a la "<<pot<<" es "<<sumaCifras(resultado)<<endl<<endl;
+    
     return 0;
 }
