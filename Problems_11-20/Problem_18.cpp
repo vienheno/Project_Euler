8cb8d2219590673b65e569940aeac760f417a2db
vienheno <herrera.victor@outlook.com>
2025-10-19 22:08:31 -0500

    Inicio de Problema 18

---
+++ Problems_11-20/Problem_18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <locale>
+#include <string>
+using namespace std;
+
+int main() {
+setlocale(LC_ALL, "es_ES.UTF-8");
+system("cls");
+
+    
+
+    return 0;
+}
