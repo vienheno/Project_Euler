08eb8ea56d57636d22c698fda3be80d080816c80
vienheno <herrera.victor@outlook.com>
2025-11-05 21:09:17 -0500

    Inicio de Problema 22

---
+++ Problems_21-30/Ejercicio_22.cpp
@@ -0,0 +1,7 @@
+// Problema 22 — Puntuación de nombres:
+// Usando el archivo 'names.txt' que contiene más de 5000 nombres, primero ordénalos alfabéticamente.
+// Luego calcula el valor alfabético para cada nombre, multiplicando esta suma por su posición en la lista
+// para obtener la puntuación del nombre.
+// Por ejemplo, cuando la lista está ordenada alfabéticamente, COLIN está en la posición 938 y su valor
+// alfabético es 3 + 15 + 12 + 9 + 14 = 53. Por lo tanto, la puntuación de COLIN es 938 × 53 = 49714.
+// ¿Cuál es la suma total de todas las puntuaciones de los nombres en el archivo?
